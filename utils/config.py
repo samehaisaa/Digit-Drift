66cb70e78be7b349fc37e992414ee3fcc21ad850
samehaisaa <a.semeh555@gmail.com>
2025-02-28 22:15:55 +0000

    add configuration parameters for image processing and model training

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+IMG_HEIGHT = 128
+IMG_WIDTH = 128
+BATCH_SIZE = 8
+LATENT_DIM = 2
+INITIAL_SHAPE = (32, 32, 64)
+EPOCHS = 5
+DATASET_DIR = 'path_to_nist_high_res_dataset'
+MODEL_SAVE_PATH = 'nist_autoencoder.h5'
